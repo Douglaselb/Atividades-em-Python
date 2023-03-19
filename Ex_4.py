9ab9c4d5c2182b2abed8d628e931966490dfe063
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-19 12:02:22 -0300

    Exercício n°4
    Atv do Curso em Vídeo

---
+++ Ex_4.py
@@ -0,0 +1,7 @@
+#Tipos primitivos
+x = input('Digite algo: ')
+print(f"""
+O tipo primitivo do ({x}) é {type(x)}
+({x}) é um caracter? {x.isalpha()}
+({x}) é um alpha númerico? {x.isnumeric()}
+""")
