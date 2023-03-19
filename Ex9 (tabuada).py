5c979f971621bcb579a1f49f4d8232994e2019ab
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-19 12:31:46 -0300

    Ex9 (tabuada)

---
+++ Ex9 (tabuada).py
@@ -0,0 +1,13 @@
+x = int(input('Digite o valor da tabuada: '))
+print(f"""
+{x} x 1 = {x*1}
+{x} x 2 = {x*2}
+{x} x 3 = {x*3}
+{x} x 4 = {x*4}
+{x} x 5 = {x*5}
+{x} x 6 = {x*6}
+{x} x 7 = {x*7}
+{x} x 8 = {x*8}
+{x} x 9 = {x*9}
+{x} x 10 = {x*10}
+""")
