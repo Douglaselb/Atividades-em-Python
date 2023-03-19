e37c3c4d18da7446a1bf91ae09b2b80e258d29c7
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-19 20:43:48 -0300

    Ex11 (area)

---
+++ Linguagem Python/Ex11 (area).py
@@ -0,0 +1,8 @@
+print('Para calcular o valor da área precisamos dos valores da altura e largura da figura.')
+x = float(input('Informe o valor da altura: '))
+y = float(input('Informe o valor do largura: '))
+print(f"""
+Altura = {x}
+Largura = {y}
+Área da figura = {(x*y):.2f}m²
+""")
