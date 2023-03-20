a4f18a4c73e16c9c22d894241c8ef6a13632abc3
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-20 18:50:27 -0300

    Ex21 (sorteio v3)

---
+++ Linguagem Python/Ex21 (sorteio v3).py
@@ -0,0 +1,8 @@
+import random
+a1 = str(input('Digite o nome do 1° aluno: '))
+a2 = str(input('Digite o nome do 2° aluno: '))
+a3 = str(input('Digite o nome do 3° aluno: '))
+a4 = str(input('Digite o nome do 4° aluno: '))
+lista = [a1,a2,a3,a4]
+random.shuffle(lista)
+print(f'Segue a sequencia de apresentação {lista}')
