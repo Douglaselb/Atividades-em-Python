11285e74fa1b365792845661d39c0798b64abcb5
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-20 15:35:20 -0300

    Ex19 (sorteio)

---
+++ Linguagem Python/Ex19 (sorteio).py
@@ -0,0 +1,8 @@
+import random
+a1 = str(input('Digite o nome do 1° aluno: '))
+a2 = str(input('Digite o nome do 2° aluno: '))
+a3 = str(input('Digite o nome do 3° aluno: '))
+a4 = str(input('Digite o nome do 4° aluno: '))
+lista = [a1,a2,a3,a4]
+escolhido = random.choice(lista)
+print(f'O aluno sorteado foi o {escolhido}')
