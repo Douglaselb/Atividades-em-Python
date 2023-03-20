cad541ac82057b8707ba651f8a3e37cee30f96c0
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-20 15:44:05 -0300

    Ex20 (sorteio v2)

---
+++ Linguagem Python/Ex20 (sorteio v2).py
@@ -0,0 +1,16 @@
+from random import randint
+print("""\tSegue a lista dos alunos que estão participando do sorteio
+[1] Ana
+[2] Clara
+[3] Danilo
+[4] Emanoel
+""")
+escolhido = randint(1,4)
+if escolhido == 1:
+    print(f'Por sorteio a Ana foi a premiada, número sorteado {escolhido}')
+elif escolhido == 2:
+    print(f'Por sorteio a Clara foi a premiada, número sorteado {escolhido}')
+elif escolhido == 3:
+    print(f'Por sorteio o Danilo foi o premiado, número sorteado {escolhido}')
+else:
+    print(f'Por sorteio o Emanoel foi o premiado, número sorteado {escolhido}')
