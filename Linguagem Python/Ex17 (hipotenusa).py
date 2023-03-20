ed04fe36c9d7ea1d6207e5f008936d7257c0402d
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-20 15:05:59 -0300

    Ex17 (hipotenusa)

---
+++ Linguagem Python/Ex17 (hipotenusa).py
@@ -0,0 +1,17 @@
+import math
+print('Para calcular a hipotenusa precisamos de algumas informações')
+oposto = float(input('Informe o valor do cateto oposto: '))
+adjacente = float(input('Informe o valor do cateto adjacente: '))
+hipotenusa = ((oposto**2)+(adjacente**2))
+hyp = math.hypot(oposto, adjacente)
+print(f"""
+F-strings
+\tCateto oposto {oposto} x {oposto} = {oposto**2}
+\tCateto adjacente {adjacente} x {adjacente} = {adjacente**2}
+\tValor da hipotenusa é {hipotenusa**0.5:.2f}
+
+Biblioteca math
+\tCateto oposto {oposto} x {oposto} = {oposto**2}
+\tCateto adjacente {adjacente} x {adjacente} = {adjacente**2}
+\tValor da hipotenusa é {hyp:.2f}
+""")
