4c7bea82bf1809dd8ac4893b1b0c16edc87d03f8
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-20 14:45:46 -0300

    Ex16 (aluguel v2)
    usando biblioteca math

---
+++ Linguagem Python/Ex16 (aluguel v2).py
@@ -0,0 +1,14 @@
+import math
+print("""Segue abaixo os valores do aluguel de carros em nossa loja:
+A cada dia é cobrado o valor de R$60 e R$0.15/KM.
+""")
+days = float(input('Nos informe quantos dias pretende permanecer com o veiculo: '))
+km = float(input('Nos informe a quilometragem que será percorrida: '))
+print(f"""
+Com as informações passadas segue seus respectivos valores:
+{days} custará R${days*60:.2f}, e {km}KM custará R${km*0.15:.2f}.
+O total do aluguel será de R${days*60+km*0.15:.2f}.
+
+Usando a função trunc {math.trunc(days*60+km*0.15)}
+Usando o modulo ceil {math.ceil(days*60+km*0.15)}
+""")
