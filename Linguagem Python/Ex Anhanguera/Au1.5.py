80213bd0fa55caf2079f7353a14d73a13492edfd
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-21 17:53:01 -0300

    While
    Estrutura de repetição usando while

---
+++ Linguagem Python/Ex Anhanguera/Au1.5.py
@@ -0,0 +1,9 @@
+#Estrutura de repetição While
+
+x = 1
+while x != 0:
+    x = int(input('Digite um valor (para sair do loop digite 0): '))
+    if x % 2 == 0:print('O número é par')
+    else:print('O número é impar')
+
+#While é usado quando não sabemos quantas vezes precisamos repetir o código
