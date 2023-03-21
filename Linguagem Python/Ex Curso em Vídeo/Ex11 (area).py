08fdd824cafcadd43329d1588010c14d7c481468
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-21 11:35:18 -0300

    Calculo de área de um ambiente
    Lado vezes altura

--- Linguagem Python/Ex Curso em Vídeo/Ex11 (area).py
+++ Linguagem Python/Ex Curso em Vídeo/Ex11 (area).py
@@ -1,3 +1,5 @@
+#Calculo simples de área
+
 print('Para calcular o valor da área precisamos dos valores da altura e largura da figura.')
 x = float(input('Informe o valor da altura: '))
 y = float(input('Informe o valor do largura: '))
