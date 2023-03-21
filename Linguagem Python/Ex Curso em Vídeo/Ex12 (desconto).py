ce723af0a2eb5f5b48ce026a0086abd2679fd8d0
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-21 11:34:08 -0300

    Desconto depende da escolha do usuário
    Valor do desconto vai até 15% do valor da compra

--- Linguagem Python/Ex Curso em Vídeo/Ex12 (desconto).py
+++ Linguagem Python/Ex Curso em Vídeo/Ex12 (desconto).py
@@ -1,11 +1,13 @@
+#Descontos a depender da escolha do usuário 
+
 print("""
 \tBem Vindo a nossa loja!
 Nossa loja trabalha com descontos a depender da opção de pagamento
 """)
 resposta = int(input("""
 \tAbaixo escolha a opção de pagamento:
-[1] A vista 15% de desconto
-[2] A vista no credito 10% de desconto
+[1] À vista 15% de desconto
+[2] À vista no credito 10% de desconto
 [3] 3x no credito, 5% de desconto
 [4] Acima de 3x, não tem desconto
 
@@ -13,9 +15,9 @@ Qual das opções você prefere:
 """))
 compras = float(input('Digite o valor total da sua compra: '))
 if resposta == 1:
-    print(f'Pagamento a vista a sua compra total fica em R${(compras-(compras * 0.15))}')
+    print(f'Pagamento à vista a sua compra total fica em R${(compras-(compras * 0.15))}')
 elif resposta == 2:
-    print(f'Pagamento a vista no credito a sua compra total fica em R${(compras-(compras * 0.1))}')
+    print(f'Pagamento à vista no credito a sua compra total fica em R${(compras-(compras * 0.1))}')
 elif resposta == 3:
     print(f'Pagamento em até 3x a sua compra total fica em R${(compras-(compras * 0.05))}')
 else:
