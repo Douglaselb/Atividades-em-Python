877a9c1e10670a19782fdeb1159b410aad2c7f6f
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-21 14:10:31 -0300

    Operadores matemáticos
    Ordens de precedência

---
+++ Linguagem Python/Ex Anhanguera/Au1.3.py
@@ -0,0 +1,15 @@
+#Operadores matemáticos
+#No python temos o operador abs() para saber o valor absoluto e o pow() para uso de potência
+
+print('2 + 3 * 5  =',2+3*5)
+print('(2 + 3) * 5 = ',(2+3)*5)
+print('2**3 * 5 = ',2**3*5)
+print('2 * 3 ** 5 = ',2*3**5)
+print("""
+Ordens de precendência em python
+Ordem em que as operações seram executadas:
+1° ()
+2° **
+3° * / // %
+4° + -
+""")
