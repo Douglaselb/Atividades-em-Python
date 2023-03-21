f7aab10436c36585ac74fad4f68aea2e28f76f17
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-21 18:27:40 -0300

    Estrutura de repetição for v2

---
+++ Linguagem Python/Ex Anhanguera/Au1.5.2.py
@@ -0,0 +1,8 @@
+#Estrutura de repetição for
+
+for c in range(1,20,2):     #range(1,10,2) inicia em 1, termina em 10, pulando de 2 em 2
+    print(c)
+    if c == 5:
+        break
+    else:
+        continue
