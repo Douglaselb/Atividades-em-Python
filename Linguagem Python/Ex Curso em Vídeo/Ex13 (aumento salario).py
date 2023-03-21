ec8674c10c457aafb1b4200a7c9f34c29b3f61bd
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-21 11:30:21 -0300

    Aumento salarial com base no salario atual
    Requisito para o aumento é receber menos de R$1.450,00

--- Linguagem Python/Ex Curso em Vídeo/Ex13 (aumento salario).py
+++ Linguagem Python/Ex Curso em Vídeo/Ex13 (aumento salario).py
@@ -1,3 +1,5 @@
+#Calculando aumento salarial de 15%, requesito: receber menos de 1.450,00
+
 print('A empresa tomou uma decisão de aumentar o salario dos funcionarios que recebem menos de R$1.450,00')
 salario = float(input('Favor informar o seu salario atual: '))
 print(f'Seu salario atual passa a ser {(salario+(salario*0.15))}') if salario < 1450 else print('Seu é acima da media.\nPor esse motivo você ficou de fora do aumento de 15%')
