061ffd119fcb311752e78c61b82cc35b225f699c
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-23 21:49:28 -0300

    Tratamento simples de strings
    fatiamento e posições

---
+++ Linguagem Python/Ex Curso em Vídeo/Ex23.py
@@ -0,0 +1,11 @@
+#tratamento de strings
+
+x = int(input('Digite um valor: '))
+y = str(x)
+print(f"""
+\tSegue as posições e valores.
+Milhar {y[0]}
+Centena {y[1]}
+Dezena {y[2]}
+Unidade {y[3]}
+""")
