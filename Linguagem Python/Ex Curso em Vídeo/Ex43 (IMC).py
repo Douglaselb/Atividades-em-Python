28ed39939d5b10c9f3991e3a0a482a42c40ce6ee
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-26 10:18:09 -0300

    Calculo IMC
    Calculo de IMC com descrição

--- Linguagem Python/Ex Curso em Vídeo/Ex43 (IMC).py
+++ Linguagem Python/Ex Curso em Vídeo/Ex43 (IMC).py
@@ -2,5 +2,13 @@
 
 altura = float(input('Digite sua altura: '))
 peso = float(input('Digite seu peso atual: '))
-imc = lambda peso, altura : (peso / (altura**2))
-print(f'Pelos dados informados o seu IMC é de {imc(peso, altura)}')
+imc = peso / (altura**2)
+if imc < 25:
+    print('IMC abaixo de 25, seu peso esta normal.')
+elif imc <= 29 and imc >= 25:
+    print('IMC entre 25 e 29, você esta com excesso de peso.')
+elif imc <= 35 and imc >= 30:
+    print('IMC entre 30 e 35, você esta obeso')
+else:
+    print('IMC acima de 35 é diagnosticado como super obesidade.')
+print(f'Pelos dados informados o seu IMC é de {imc:.2f}')
