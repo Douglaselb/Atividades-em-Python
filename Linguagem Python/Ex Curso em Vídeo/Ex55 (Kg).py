da723b0f62cd7b7115992bf633f5e7cb7ea12db4
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-03-27 17:30:15 -0300

    if max e min
    print do maior kg e menor kg

--- Linguagem Python/Ex Curso em Vídeo/Ex55 (Kg).py
+++ Linguagem Python/Ex Curso em Vídeo/Ex55 (Kg).py
@@ -1,14 +1,14 @@
 #print a pessoa com maior e menor peso
-maior = 0
-menor = 0
+max = 0
+min = 0
 for c in range(1,6):
     kg = float(input(f'Informe o peso da {c}° pessoa: '))
     if c == 1:
         maior = c
         menor = c
     else:
-        if kg > maior:
-            maior = kg
-        elif kg < menor:
-            menor = kg
-print(f'A pessoa com maior peso tem {maior}Kg\nE o menor tem {menor}Kg')
+        if kg > max:
+            max = kg
+        elif kg < min:
+            min = kg
+print(f'A pessoa com maior peso tem {max}Kg\nE o menor tem {min}Kg')
