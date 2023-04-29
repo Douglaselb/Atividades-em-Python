523303fab45f7f8f7706e0a3294b775d261a352d
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-04-28 21:51:09 -0300

    Fatiamento de strings

---
+++ Linguagem Python/Exercicios aleatorios/Strings/Ex1.py
@@ -0,0 +1,10 @@
+texto = 'hellow world'
+print(f'''Exemplos de fatiamento de string
+texto[2:] {texto[2:]}
+texto[:6] {texto[:6]}
+texto[2:6] {texto[2:6]}
+texto[-2:] {texto[-2:]}
+texto[:-6] {texto[:-6]}
+texto[::-1] {texto[::-1]}
+texto[::2] {texto[::2]}
+''')
