f2655dfc01fb2c0f114cd1502c80c1bd5acaae40
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-24 17:50:15 -0300

    Exer.02

--- Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex2.py
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex2.py
@@ -1,2 +1,3 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 num = int(input('Digite um numero entre [1/4]: '))
 print(f'O numero digitado foi o {num}.')
