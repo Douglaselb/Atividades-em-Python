83068bad0b2981f91b5ac4750c210509c51d77cc
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-24 17:45:13 -0300

    Exer.01

---
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex1.py
@@ -0,0 +1,18 @@
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+print('Diferentes maneiras de printar o Ola mundo.')
+texto = 'Ola Mundo'
+tex = 'Ola '
+to = 'Mundo'
+print(f"""Digitando o texto no print:
+Ola mundo
+
+Criando uma variavel:
+{texto}
+
+Concatenando duas variaveis:
+{tex+to}
+
+Usando o input:
+""")
+var = str(input('Digite o texto "Ola Mundo" sem aspas: '))
+print(var)
