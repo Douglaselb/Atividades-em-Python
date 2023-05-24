d8f19e472f1491a88b7c035b743855a686b7577f
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-24 17:52:47 -0300

    Exer.04 Média de dois números

---
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex4.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.,
+num1 = int(input('Digite o valor do primeiro numero: '))
+num2 = int(input('Digite o valor do segundo numero: '))
+print(f'A soma de {num1} + {num2} = {num1+num2}, e sua media é {(num1+num2)/2}')
