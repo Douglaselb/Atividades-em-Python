aa39cc767e3fd32dff30db56cf77728daa5a51b1
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-24 17:55:02 -0300

    Exer.02 - Ler um número

--- Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex2.py
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex2.py
@@ -1,3 +1,4 @@
 #Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
 num = int(input('Digite um numero entre [1/4]: '))
 print(f'O numero digitado foi o {num}.')
