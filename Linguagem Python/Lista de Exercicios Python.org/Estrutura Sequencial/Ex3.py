68b0dcb963f4a0019a13383bccfcbb071ed4fade
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-24 17:49:28 -0300

    Exer.03

---
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex3.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça dois números e imprima a soma.
+num1 = int(input('Digite o valor do primeiro numero: '))
+num2 = int(input('Digite o valor do segundo numero: '))
+print(f'A soma de {num1} + {num2} = {num1+num2}')
