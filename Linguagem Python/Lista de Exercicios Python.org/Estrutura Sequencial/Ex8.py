13fc4353661f0104e4420b92f498e0f7e4d72be7
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-25 14:51:40 -0300

    Exer.08 - Salario por hora trabalhada

---
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura Sequencial/Ex8.py
@@ -0,0 +1,4 @@
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+sal = float(input('Informe quanto você recebe por hora trabalhada: '))
+hr = float(input('Informe quantas horas você trabalha por mês: '))
+print(f'O seu salario com as informações repassadas será de R${sal*hr}.')
