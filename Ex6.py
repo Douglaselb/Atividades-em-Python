697013bae40e47687d2ffe75f11a9feb9b97e6c7
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-25 14:22:52 -0300

    Exer.6 - Área de um circulo

---
+++ Ex6.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = float(input('Agora calcularemos a area de um circulo, para tal calculo precisamos apenas saber o raio,\nFavor informe o valor do raio em metros: '))
+pi = 3.14
+print(f'A area do circulo é de {pi*(raio**2):.2f}')
