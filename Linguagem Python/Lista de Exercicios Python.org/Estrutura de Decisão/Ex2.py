dd6d6b9f66512a0fe3e8d7487657364c38a82304
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-05-26 16:26:05 -0300

    Exer.02 - Positivo ou Negativo

---
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura de Decisão/Ex2.py
@@ -0,0 +1,3 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+num1 = float(input('Informe um valor: '))
+print('O número informado é positivo.') if num1 > 0.0 else print('O valor é negativo.')
