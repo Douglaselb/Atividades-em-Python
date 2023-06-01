1d5b79c1a2f0185d2e9f3358a7ef2da1552d8bcb
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-01 11:42:33 -0300

    Exer.6 - Contagem crescente

---
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura de Repetição/Ex6.py
@@ -0,0 +1,5 @@
+#Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+for c in range(1,21):
+    print(c)
+for x in range(1,21):
+    print(x,end=' ')
