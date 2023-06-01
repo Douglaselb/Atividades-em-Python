55f564f62e372bb2c51a300a14f4860a89ac7ed1
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-01 11:51:01 -0300

    Exer.6 - Maior de três

--- Linguagem Python/Lista de Exercicios Python.org/Estrutura de Decisão/Ex6.py
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura de Decisão/Ex6.py
@@ -1,6 +1,10 @@
 #Faça um Programa que leia três números e mostre o maior deles
-numeros = []
-for c in range(1,4):
-    numero = int(input(f'Informe o {c}\u00b0 número: '))
-    numeros.append(numero)
-print(f'O maior foi o {max(numeros)}')
+n1 = float(input('Informe o primeiro número: '))
+n2 = float(input('Informe o segundo número: '))
+n3 = float(input('Informe o terceiro número: '))
+if n1 > n2 and n1 > n3:
+    print(f'O maior número adicionado foi o {n1}.')
+elif n2 > n1 and n2 > n3:
+    print(f'O maior número adicionado foi o {n2}')
+else:
+    print(f'O maior número adicionado foi o {n3}.')
