56cf8fcb45aa0217baac241fedaefac3610d5727
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-02 14:37:21 -0300

    Exer.7 - Maior e menor

--- Linguagem Python/Lista de Exercicios Python.org/Estrutura de Decisão/Ex7.py
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura de Decisão/Ex7.py
@@ -1,6 +1,13 @@
 #Faça um Programa que leia três números e mostre o maior e o menor deles.
-numeros = []
+'''numeros = []
 for c in range(1,4):
     numero = int(input(f'Informe o {c}\u00b0 número: '))
     numeros.append(numero)
-print(f'O maior foi o {max(numeros)}, o menor foi o {min(numeros)}.')
+print(f'O maior foi o {max(numeros)}, o menor foi o {min(numeros)}.')'''
+
+n1 = float(input('Informe o primeiro número: '))
+n2 = float(input('Informe o segundo número: '))
+n3 = float(input('Informe o terceiro número: '))
+maior = max(n1, n2, n3)
+menor = min(n1, n2, n3)
+print(f'O maior é o {maior} e o menor é o {menor}.')
