da0dee8402c587c73bfb57c799bd45cf0714295e
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-02 14:44:48 -0300

    Exer.9 - De 1 até 50

---
+++ Linguagem Python/Lista de Exercicios Python.org/Estrutura de Repetição/Ex9.py
@@ -0,0 +1,9 @@
+#Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+var = 1
+print('Usando While:')
+while var < 51:
+    print(var, end=' ')
+    var += 1
+print('\nUsando For:')
+for c in range(51):
+    print(c, end=' ')
