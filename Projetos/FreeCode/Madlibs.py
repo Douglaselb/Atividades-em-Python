0dc69a03925a8af0ec8cd6d2096b051a2d41ffcd
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-07 16:15:57 -0300

    Mad Libs

---
+++ Projetos/FreeCode/Madlibs.py
@@ -0,0 +1,34 @@
+'''Segundo o Wikipedia Mad Libs é um jogo de palavras de modelo de frase criado por Leonard Stern e Roger Price. Consiste em um jogador solicitando aos outros uma lista de palavras para substituir os espaços em branco em uma história antes de ler em voz alta.'''
+print('Para darmos o pontapé em nossa história preencha algumas informações.')
+persona1 = str(input('Informe o nome do 1\u00b0 personagem: '))
+persona2 = str(input('Informe o nome do 2\u00b0 personagem: '))
+persona3 = str(input('Informe o nome do 3\u00b0 personagem: '))
+verbo1 = str(input('Informe o 1\u00b0 verbo: '))
+verbo2 = str(input('Informe o 2\u00b0 verbo: '))
+verbo3 = str(input('Informe o 3\u00b0 verbo: '))
+verbo4 = str(input('Informe o 4\u00b0 verbo: '))
+verbo5 = str(input('Informe o 5\u00b0 verbo: '))
+adj1 = str(input('Informe o 1\u00b0 adjetivo: '))
+adj2 = str(input('Informe o 2\u00b0 adjetivo: '))
+adj3 = str(input('Informe o 3\u00b0 adjetivo: '))
+adj4 = str(input('Informe o 4\u00b0 adjetivo: '))
+adj5 = str(input('Informe o 5\u00b0 adjetivo: '))
+print(f'''A Aventura do {persona1}, {persona2} e {persona3}
+
+Era uma vez, em uma cidade bem distante, viviam três personagens muito especiais. O(a) {persona1}, o(a) {persona2} e o(a) {persona3} eram conhecidos por sua curiosidade e espírito aventureiro.
+
+Um belo dia, os três amigos decidiram explorar uma misteriosa floresta próxima à cidade. O ambiente estava repleto de árvores {adj1} e criaturas {adj2}. Eles estavam tão animados que começaram a {verbo1} e {verbo2} freneticamente, deixando todos os pássaros ao redor assustados.
+
+Enquanto caminhavam mais profundamente na floresta, depararam-se com uma clareira encantadora. A grama era tão {adj3} que parecia um tapete macio sob seus pés. Eles se sentaram e começaram a {verbo3} enquanto compartilhavam histórias engraçadas.
+
+De repente, ouviram um som estranho vindo de uma moita próxima. Curiosos como sempre, decidiram investigar. Com coragem, eles se aproximaram lentamente, prontos para enfrentar qualquer coisa. Para surpresa deles, encontraram um(a) {adj4} e {adj5} coelho, que parecia estar dançando!
+
+Os três amigos riram tanto que não conseguiram parar. Eles se juntaram ao coelho em uma dança desajeitada e divertida. Parecia que o coelho entendia seus movimentos e dançava ainda mais animadamente.
+
+Depois de um tempo, exaustos de tanto {verbo5}, eles decidiram retornar à cidade. Embora cansados, tinham memórias incríveis dessa aventura na floresta encantada.
+
+E assim, o(a) {persona1}, o(a) {persona2} e o(a) {persona3} continuaram a viver suas vidas cheias de risos, aventuras e amizade.
+
+Fim.
+''')
+
