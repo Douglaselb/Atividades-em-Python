05a9b144d04763c22d0e8f8a6994652612c87ec4
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-15 23:39:24 -0300

    Enumerate

---
+++ Linguagem Python/Exercicios aleatorios/Numpy/Enumerate.py
@@ -0,0 +1,7 @@
+# Método repassa a possição de cada elemento na iteração
+
+import numpy as np
+
+matriz = np.array([[1,2,3],[4,5,6]])
+for posicao, elemento in np.ndenumerate(matriz):
+    print(posicao, elemento)
