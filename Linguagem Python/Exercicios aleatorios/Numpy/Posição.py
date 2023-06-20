aaa2a91cf08997a1234202c05fa36a17aa4b3e9d
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-20 20:26:40 -0300

    where()
    Localizando determinados valores de uma array

--- Linguagem Python/Exercicios aleatorios/Numpy/Posição.py
+++ Linguagem Python/Exercicios aleatorios/Numpy/Posição.py
@@ -11,3 +11,9 @@ print('Posição do valor 9 na array 1-D:\n', local_um)
 array_dois = np.array([[1,2,3],[9,8,7]])
 local_dois = np.where(array_dois == 9)
 print('\nPosição do valor 9 na array 2-D:\n', local_dois)
+
+print('Array original:\n', array_um)
+pares = np.where(array_um%2 == 0) # Localizando a posição dos elementos de valores pares
+impares = np.where(array_um%2 == 1) # Localizando a posição dos elementos de valores impares
+print('\nPosição dos numeros pares:\n', pares)
+print('\nPosição dos numeros pares:\n', impares)
