1fde7913be4bd7936cec709b13ee39c670a2718e
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-22 21:10:04 -0300

    Filtragem
    Método para filtrar elementos de uma array com condições especificas

--- Linguagem Python/Exercicios aleatorios/Numpy/Filter.py
+++ Linguagem Python/Exercicios aleatorios/Numpy/Filter.py
@@ -14,6 +14,15 @@ for elementos in array_original:
         condicao_dois.append(False)
 filtro_dois = array_original[condicao_dois] # Filtro dos números maiores que 25
 
+condicao_tres = []
+for elemento in array_original:
+    if elemento % 2 == 0:
+        condicao_tres.append(True)
+    else:
+        condicao_tres.append(False)
+filtro_tres = array_original[condicao_tres]
+
 print('Array original: ', array_original)
 print('Valores com condição True', filtro)
 print('Números maiores que 25: ', filtro_dois)
+print('Números pares: ', filtro_tres)
