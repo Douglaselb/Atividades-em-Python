a72c80a067b7989b8b3416124397aea452d84a2d
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-26 18:46:04 -0300

    nditer()
    Continuação da iteração

--- Linguagem Python/Exercicios aleatorios/Numpy/Iterar_v2.py
+++ Linguagem Python/Exercicios aleatorios/Numpy/Iterar_v2.py
@@ -1,12 +1,12 @@
-# Continuação da iteração de matrizes
+# Continuação da iteração de array
 
 import numpy as np
 
-matriz = np.array([[[1,2,3],[4,5,6]],[[11,12,13],[14,15,16]]])
-segunda_matriz = np.array([1,2,3,4,5])
-# Fazendo uso do ndtiter() não é preciso fazer tantos loops
-for x in np.nditer(matriz):
+array = np.array([[[1,2,3],[4,5,6]],[[11,12,13],[14,15,16]]])
+segunda_array = np.array([1,2,3,4,5])
+# Fazendo uso do nditer() não é preciso fazer tantos loops
+for x in np.nditer(array):
     print(x, end='  ')
 print('\nUsando flag e op_dtype')
-for y in np.nditer(matriz, flags=['buffered'], op_dtypes= [float]): # O op_dtype não altera a matriz original
+for y in np.nditer(array, flags=['buffered'], op_dtypes= [float]): # O op_dtype não altera a array original
     print(y, end='  ')
