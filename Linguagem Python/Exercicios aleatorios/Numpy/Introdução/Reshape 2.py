ed125093933795a8079677d5a951c828d97cfe7a
Douglas Barros <107780075+Douglaselb@users.noreply.github.com>
2023-06-29 18:34:17 -0300

    reshape() 2
    Continuação do reshape()

--- Linguagem Python/Exercicios aleatorios/Numpy/Introdução/Reshape 2.py
+++ Linguagem Python/Exercicios aleatorios/Numpy/Introdução/Reshape 2.py
@@ -2,17 +2,17 @@
 
 import numpy as np
 
-matriz = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+array = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 '''
 Neste caso o terceiro valor permite que o numpy determine o tamanho da terceira dimensão
-com base nos valores anteriores e o total da matriz original
+com base nos valores anteriores e o total da array original
 '''
-modificada = matriz.reshape(2, 2, -1)
-print('Matriz original\n', modificada.base)
+modificada = array.reshape(2, 2, -1)
+print('array original\n', modificada.base)
 print('\nTerceira modficada:\n', modificada)
 
 '''
-    O valor -1 faz a matriz n-D ser uma matriz 1-D
+    O valor -1 faz a array n-D ser uma array 1-D
     Outra maneira que apresenta o mesmo resultado é o método flatten() e o ravel()
 '''
 modificada = modificada.reshape(-1)
